c835a84574a00999a971865868395d2ca50ad9cc
juniornoodle <35826104+juniornoodle@users.noreply.github.com>
2018-08-18 14:26:29 -0400

    Updated phue.py

--- phue.py
+++ phue.py
@@ -1,6 +1,6 @@
 #This is written for Python3+ so make sure that is the version you are using!
 #Python should have the JSON library by default. However the 'requests' library will need to be installed.
-#This is pretty dependent and differs for each OS. Linux, for example, is just 'pip install requests'
+#This is pretty dependent and differs for each OS. Linux, for example, is just 'python3 -m pip install requests'
 import json, requests
 
 #The URL which is needed for the PUT requests. In this case it is only one light
